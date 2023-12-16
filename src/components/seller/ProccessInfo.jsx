e61f99aea10f32549ce80e9e839ca0147f98778c
Akshay <akalekar2003@gmail.com>
2023-12-16 18:34:46 +0530

    hello

--- src/components/seller/ProccessInfo.jsx
+++ src/components/seller/ProccessInfo.jsx
@@ -6,15 +6,12 @@ const ProccessInfo = () => {
       <h1 className="text-5xl font-bold ">
         Join the eZ revolution, and start monetizing your solutions !
       </h1>
-      <div className="m-auto w-fit mt-16">
-        <div className="flex flex-col lg:flex-row justify-between gap-8">
-        {/**card1**/}
-          <div className="card w-96 p-0 shadow-xl rounded-t-xl">
-              <div
-                className="h-[4rem] w-full bg-orange-400 rounded-t-xl "
-              >
-              </div>
-            
+      <div className="m-auto w-4/5 mt-16">
+        <div className="flex flex-wrap lg:flex-nowrap justify-between gap-8">
+          {/**card1**/}
+          <div className="card  flex-1 p-0 shadow-xl rounded-t-xl">
+            <div className="h-[4rem] w-full bg-orange-400 rounded-t-xl "></div>
+
             <div className="card-body text-left">
               <h2 className="card-title text-[#146B1E]">Sign up:</h2>
               <p>
@@ -23,33 +20,35 @@ const ProccessInfo = () => {
               </p>
             </div>
           </div>
-          <div className="card w-96 p-0 shadow-xl rounded-t-xl">
-          <div
-            className="h-[4rem] w-full bg-[#ABA6A2] rounded-t-xl "
-          >
-          </div>
-        
-        <div className="card-body text-left">
-          <h2 className="card-title text-[#146B1E]">Set up your profile: </h2>
-          <p>
-          This is where you showcase your skills, experience, education, and achievements. Make your profile more attractive and professional.
-          </p>
-        </div>
-      </div>
-      <div className="card w-96 p-0 shadow-xl rounded-t-xl">
-      <div
-        className="h-[4rem] w-full bg-[#5AC96C] rounded-t-xl "
-      >
-      </div>
-    
-    <div className="card-body text-left">
-      <h2 className="card-title text-[#146B1E]">List your services: </h2>
-      <p>
-      Sell your have that people might need, and create listings that describe your services in detail. You can set your own prices, and offer different packages or add-ons to make your services more attractive.
+          <div className="card flex-1 p-0 shadow-xl rounded-t-xl">
+            <div className="h-[4rem] w-full bg-[#ABA6A2] rounded-t-xl "></div>
 
-      </p>
-    </div>
-  </div>
+            <div className="card-body text-left">
+              <h2 className="card-title text-[#146B1E]">
+                Set up your profile:{" "}
+              </h2>
+              <p>
+                This is where you showcase your skills, experience, education,
+                and achievements. Make your profile more attractive and
+                professional.
+              </p>
+            </div>
+          </div>
+          <div className="card flex-1 p-0 shadow-xl rounded-t-xl">
+            <div className="h-[4rem] w-full bg-[#5AC96C] rounded-t-xl "></div>
+
+            <div className="card-body text-left">
+              <h2 className="card-title text-[#146B1E]">
+                List your services:{" "}
+              </h2>
+              <p>
+                Sell your have that people might need, and create listings that
+                describe your services in detail. You can set your own prices,
+                and offer different packages or add-ons to make your services
+                more attractive.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
